b91922d2e61dbf9dd7f8c832eae58d1bfa8f72c4
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2025-10-17 00:50:54 +0300

    date->person

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 
 using namespace std;
 
-class Data{
+class Person{
 private:
-    // int day;
-    // int month;
-    // int year;
+    int day_;
+    int month_;
+    int year_;
+    string name_;
+    string surname_;
 
     bool isValid(int &day, int &month, int &year){
         const chrono::year_month_day ymd((chrono::year(year)), (chrono::month(month)), (chrono::day(day)));
@@ -16,28 +18,36 @@ private:
             return true;
         else
             return false;
-        }
+    }
+    
 public:
-    Data(int d, int m, int y){ 
+    Person(string n, string sn, int d, int m, int y) : name_(n), surname_(sn){ 
             if(!isValid(d, m, y))
                 throw "Not valid data!";
-            else{
-                day = d;
-                month = m;
-                year = y;
-            }
+
+            day_ = d;
+            month_ = m;
+            year_ = y;
     };
 
-    int day;
-    int month;
-    int year;
+    int day() const{ return day_; };
+    int month() const{ return month_; };
+    int year() const{ return year_; };
+    string name() const{ return name_; };
+    string surname() const{ return surname_; };
+    string getData() const{ return to_string(day_) + '.' + to_string(month_) + '.' + to_string(year_); };
 };
+
+ostream& operator<<(ostream& stream, const Person& pers){
+    stream << pers.name() << ' ' << pers.surname() << ' ' << pers.getData();
+    return stream;
+}
 
 int main(){
     ifstream in;
     in.open("text.txt");
 
-    vector<Data> dates;
+    vector<Person> perses;
 
     if(in.is_open()){
         string line;
@@ -45,8 +55,9 @@ int main(){
         while(getline(in, line)){
             try{
                 int day, month, year;
-                sscanf(line.c_str(), "%*d %*s %*s %d.%d.%d", &day, &month, &year);
-                dates.emplace_back(day, month, year);
+                char buffer_name[50], buffer_surname[50];
+                sscanf(line.c_str(), "%*d %s %s %d.%d.%d", &buffer_name, &buffer_surname, &day, &month, &year);
+                perses.emplace_back(buffer_name, buffer_surname, day, month, year);
             }
             catch(const char* msg){
                 cout << msg << '\n';
@@ -54,12 +65,12 @@ int main(){
         }
     }
     else
-        cout << "Error\n";
+        cout << "Not open\n";
 
     in.close();
 
-    for(auto &dt : dates)
-        cout << dt.day << ' ' << dt.month << ' ' << dt.year << '\n';
+    for(const auto& ps: perses)
+        cout << ps << '\n';
 
     return 0;
 }
